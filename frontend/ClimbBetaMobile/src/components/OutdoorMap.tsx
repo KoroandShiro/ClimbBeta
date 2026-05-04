11441ea82cfc635274335ee6fcba490de39403a4
RubenLimaDuarte <rubenalduarte24@gmail.com>
2026-05-04 22:28:35 +0100

    Revert "Outdoor frontend related"
    This reverts commit a0f43c172dd5c6eb47bdc29f9410e6a117cc3541.

--- frontend/ClimbBetaMobile/src/components/OutdoorMap.tsx
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'react-native';
-import OutdoorMapWeb from './OutdoorMap.web';
-import OutdoorMapNative from './OutdoorMap.native';
-
-const OutdoorMap = Platform.OS === 'web' ? OutdoorMapWeb : OutdoorMapNative;
-
-export default OutdoorMap;
--- frontend/ClimbBetaWeb/src/App.tsx
+++ frontend/ClimbBetaWeb/src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard'; // <-- Importamos o Dashboard a sério!
-import  Outdoor from './pages/Outdoor';
-import CreateOutdoorRoute from './pages/CreateOutdoorRoute';
-import OutdoorDetails from './pages/OutdoorDetails';
 
 function App() {
   return (
@@ -11,10 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/gyms" element={<Dashboard />} />
-          <Route path = "/outdoor" element = {<Outdoor />} />
-          <Route path = "/outdoor/create" element = {<CreateOutdoorRoute />} />
-          <Route path = "/outdoor/:id" element = {<OutdoorDetails />} />
+        <Route path="/gyms" element={<Dashboard />} /> 
       </Routes>
     </BrowserRouter>
   );
